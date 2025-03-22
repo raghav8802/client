cdb7ed268ef62720f7a4f41a7652c020e4f36f26
Raghav <54115719+raghav8802@users.noreply.github.com>
2025-03-22 15:17:20 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,41 +1,41 @@
-// import { NextResponse } from 'next/server';
-// import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-// export async function middleware(request: NextRequest) {
-//     const path = request.nextUrl.pathname;
+export async function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname;
 
-//     if (path.startsWith("/_next")) {
-//         return NextResponse.next();
-//     }
+    if (path.startsWith("/_next")) {
+        return NextResponse.next();
+    }
 
-//     const isPublicPath = path === '/xg6jtv54ghv' || path === '/signin' || path === '/register' || path === '/verifyemail' || path === '/forgotpassword' || path === '/agreement' || path === '/reset-password' || path === '/message';
+    const isPublicPath = path === '/xg6jtv54ghv' || path === '/signin' || path === '/register' || path === '/verifyemail' || path === '/forgotpassword' || path === '/agreement' || path === '/reset-password' || path === '/message';
 
-//     const token = request.cookies.get('token')?.value || '';
+    const token = request.cookies.get('token')?.value || '';
  
-//     // Allow API routes to bypass the token check
-//     if (path.startsWith('/api')) {
-//         return NextResponse.next();
-//     }
+    // Allow API routes to bypass the token check
+    if (path.startsWith('/api')) {
+        return NextResponse.next();
+    }
 
-//     if (token && isPublicPath) {
-//         return NextResponse.redirect(new URL('/', request.url));
-//     }
+    if (token && isPublicPath) {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
 
-//     if (!token && !isPublicPath) {
-//         return NextResponse.redirect(new URL('/signin', request.url));
-//     }
+    if (!token && !isPublicPath) {
+        return NextResponse.redirect(new URL('/signin', request.url));
+    }
 
-//     return NextResponse.next();
-// }
+    return NextResponse.next();
+}
 
-// // See "Matching Paths" below to learn more
-// export const config = {
-//     matcher: [
-//         '/signin',
-//         '/register',
-//         '/verifyemail',
-//         '/forgotpassword',
-//         '/((?!api|_next/static|_next/image|favicon.ico).*)',
-//         '/:path*',
-//     ]
-// };
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: [
+        '/signin',
+        '/register',
+        '/verifyemail',
+        '/forgotpassword',
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+        '/:path*',
+    ]
+};
