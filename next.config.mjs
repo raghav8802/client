ed02fcacd018024f3538fc2a316be479cf7a78bb
Raghav <54115719+raghav8802@users.noreply.github.com>
2025-03-25 16:59:00 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'swalay-music-files.s3.ap-south-1.amazonaws.com',
-          pathname: '/**',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'swalay-music-files.s3.ap-south-1.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '100mb' // Set your desired limit (100MB in this case)
+    }
+  }
+};
+
+export default nextConfig;
   
